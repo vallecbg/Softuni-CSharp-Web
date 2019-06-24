0f5cc33970c8f9d842ac3aa0e0a971f5c44daaee
vallecbg <valioplayofficial@gmail.com>
2019-06-25 02:25:06 +0300

    Added FDMC

---
+++ ASP.NET Core/EXERCISE ASP.NET CORE INTRODUCTION/Fdmc/Fdmc/Data/ConnectionOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fdmc.Data
+{
+    public class ConnectionOptions
+    {
+        public const string ConnectionString =
+            "Server=.\\SQLEXPRESS;Database=FdmcDB;Trusted_Connection=True;MultipleActiveResultSets=true;";
+    }
+}
